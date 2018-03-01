22a45d0f214e2adf2e9abfbb94db3f101e906605
Radu Selea <raduselea@gmail.com>
2018-03-01 23:35:50 +0200

    added main tracking activity and code style

---
+++ app/src/main/java/raduselea/intermitentfasttracking/com/intermitentfasttracking/TrackMainActivity.java
@@ -0,0 +1,15 @@
+package raduselea.intermitentfasttracking.com.intermitentfasttracking;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TrackMainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_track_main);
+	}
+}
